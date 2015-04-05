32084ee312bf3bc6b7296beafea4b553ce689a16
OndrejRitomsky <o.ritomsky@gmail.com>
2015-04-05 19:47:13 +0200

    def. basic tools finished, little refactor

--- assets/js/SharedCanvas.js
+++ assets/js/SharedCanvas.js
@@ -1,48 +1,172 @@
 function SharedCanvas(){
-  this.CANVAS_WIDTH = 800;
-  this.CANVAS_HEIGHT = 600;
-  
-  this.CURSOR_PEN = 0;
-  this.CURSONR_RUBBER = 1;
-  
-  this.COLOR_MIN_VAL = 0;
-  this.COLOR_MAX_VAL = 255;
-  
-  this.COLOR_RED = "RED";
-  this.COLOR_GREEN = "GREEN";
-  this.COLOR_BLUE = "BLUE";
+  this.cursorType = 0;
+  this.cursorWidth = 3;
+  this.colorRed = 0;
+  this.colorGreen = 0;
+  this.colorBlue = 0;
+
+  this.canvas = null;
+  this.context = null;
+
+  this.counter = 0;
+  this.mouseEvt = null;
+  this.oldPos = null;
+  this.firedOnce = false;
+  this.mousedownID = -1;
 }
 
-SharedCanvas.prototype.cursorType = 0;
-SharedCanvas.prototype.colorRed = 0;
-SharedCanvas.prototype.colorGreen = 0;
-SharedCanvas.prototype.colorBlue = 0;
 
-SharedCanvas.prototype.context = null;
+SharedCanvas.CURSOR_DRAWING_UPDATE_RATE = 10;
 
-/*SharedCanvas.prototype.myoffsetLeft = 0;
-SharedCanvas.prototype.myoffsetTop = 0;*/
+SharedCanvas.CANVAS_WIDTH = 800;
+SharedCanvas.CANVAS_HEIGHT = 600;
 
-SharedCanvas.prototype.initialization = function(context){
-  this.context = context;
+SharedCanvas.CURSOR_PEN = 0;
+SharedCanvas.CURSONR_RUBBER = 1;
+
+SharedCanvas.COLOR_MIN_VAL = 0;
+SharedCanvas.COLOR_MAX_VAL = 255;
+
+SharedCanvas.COLOR_RED = "RED";
+SharedCanvas.COLOR_GREEN = "GREEN";
+SharedCanvas.COLOR_BLUE = "BLUE";
+
+
+
+SharedCanvas.prototype.initialization = function(canvas){
+
+  this.canvas = canvas;
+  this.context = this.canvas.getContext("2d");
+  
+  this.context.lineCap= "round";
+  this.context.lineJoin= "mitter"; 
+  //cxt.lineCap= "butt|round|square";
+  //cxt.lineJoin= "round|bevel|miter"; 
 
 }
-
 
 SharedCanvas.prototype.changeColor = function(color, value){
-  if (value<this.COLOR_MIN_VAL || value>this.COLOR_MAX_VAL)
-    return;
+  if (value < SharedCanvas.COLOR_MIN_VAL)
+    return SharedCanvas.COLOR_MIN_VAL;
+  if (value > SharedCanvas.COLOR_MAX_VAL)
+    return SharedCanvas.COLOR_MAX_VAL;
   
   switch(color) {
-    case this.COLOR_RED:
+    case SharedCanvas.COLOR_RED:
         this.colorRed = value;
-        break;
-    case this.COLOR_GREEN:
+        return value;
+    case SharedCanvas.COLOR_GREEN:
         this.colorGreen = value;
-        break;
-    case this.COLOR_BLUE:
+        return value;
+    case SharedCanvas.COLOR_BLUE:
         this.colorBlue = value;
-        break;
+        return value;
   } 
+  
+  return this.COLOR_MIN_VAL;
+  
 }
 
+SharedCanvas.prototype.getMousePos = function(evt) {
+  var rect = this.canvas.getBoundingClientRect();
+  return {
+    x:  Math.round((evt.clientX-rect.left)/(rect.right-rect.left)*canvas.width) +0.5,
+    y:  Math.round((evt.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height) +0.5
+  };  
+}
+
+
+SharedCanvas.prototype.whilemousedown = function() {
+
+  if (this.mouseEvt!=null){
+  
+    
+    var pos = this.getMousePos(this.mouseEvt);
+    
+    if (!this.firedOnce){
+      this.context.beginPath();
+      this.context.moveTo(pos.x, pos.y); 
+      
+      this.counter = (this.counter + 1) % 2;
+      this.firedOnce = true;  
+      
+      return;
+    }
+    
+    if (this.counter==0){
+      this.context.beginPath();
+      
+      if (this.oldPos!=null)
+        this.context.moveTo(this.oldPos.x, this.oldPos.y); 
+      else 
+        this.context.moveTo(pos.x, pos.y); 
+    }
+    
+    if (this.counter==1){
+      this.context.lineTo(pos.x, pos.y);
+      this.context.stroke(); 
+      
+      this.oldPos=pos;
+    }
+    
+    this.counter = (this.counter + 1) % 2;
+    //point(pos.x,pos.y,this.context);
+  }
+}
+
+
+
+
+SharedCanvas.prototype.onMouseDown = function(event) {
+  
+  if(this.mousedownID==-1){
+    
+     // DRAWING SETUP 
+     this.context.lineWidth = this.cursorWidth;  
+     this.context.strokeStyle = 'rgba('+this.colorRed+','+this.colorGreen+','+this.colorBlue+',1)';
+    
+     // -------------------
+     var t = this;
+     this.mousedownID = setInterval(function(){t.whilemousedown();}, SharedCanvas.CURSOR_DRAWING_UPDATE_RATE);
+  }
+}
+
+SharedCanvas.prototype.onMouseUp = function(event) {
+  if(this.mousedownID!=-1) {  //Only stop if exists
+     clearInterval(this.mousedownID);
+     this.mousedownID=-1;
+     if (this.counter==1){
+       this.whilemousedown();  
+     }
+
+     this.counter = 0;
+     this.context.stroke();
+     this.oldPos = null;
+     this.firedOnce = false;
+   }
+}
+
+SharedCanvas.prototype.onMouseMove = function(event) { 
+  this.mouseEvt = event;
+}
+
+
+// ---------------------------------------
+/*
+var canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+
+// That's how you define the value of a pixel //
+function drawPixel (x, y, r, g, b, a) {
+    var index = (x + y * canvasWidth) * 4;
+
+    canvasData.data[index + 0] = r;
+    canvasData.data[index + 1] = g;
+    canvasData.data[index + 2] = b;
+    canvasData.data[index + 3] = a;
+}
+
+// That's how you update the canvas, so that your //
+// modification are taken in consideration //
+function updateCanvas() {
+    ctx.putImageData(canvasData, 0, 0);
+}*/
--- assets/js/canvasTools.js
+++ assets/js/canvasTools.js
@@ -1,9 +1,6 @@
 var sharedCanvas;
 
-
-var ctx;
 $(document).ready(function() {
-  console.log("ready!");
   
   var canvas = $("#canvas")[0];
   if (!canvas)
@@ -11,88 +8,78 @@ $(document).ready(function() {
   
   canvas.width  = 800;
   canvas.height = 600; 
-  canvas.style.width  = '1024px';
-  canvas.style.height = '768px';
-  console.log(canvas);
-  ctx = canvas.getContext("2d");
-  /*Create SharedCanvas instace*/
+  canvas.style.width  = '800px';
+  canvas.style.height = '600px';
+  
+  /*Create SharedCanvas instace and init*/
   sharedCanvas = new SharedCanvas();
-  sharedCanvas.initialization(canvas.getContext("2d"));
+  if (!sharedCanvas)
+    return;
+  
+  sharedCanvas.initialization(canvas);
+  
+  /*Reset Inputs*/
+  $('#canvasRed').val(sharedCanvas.colorRed);
+  $('#canvasGreen').val(sharedCanvas.colorGreen);
+  $('#canvasBlue').val(sharedCanvas.colorBlue);
+  sharedCanvas.cursorWidth = $('#canvasCursorWidth').val();
+  showInputColor(sharedCanvas.colorRed,sharedCanvas.colorGreen,sharedCanvas.colorBlue);
+  
+  /*Add Input Events*/
+  $('#canvasRed').bind('input', function(){
+    checkInput(this, SharedCanvas.COLOR_RED);
+    showInputColor(sharedCanvas.colorRed,sharedCanvas.colorGreen,sharedCanvas.colorBlue);
+  });
+  $('#canvasGreen').bind('input', function(){
+    checkInput(this, SharedCanvas.COLOR_GREEN);
+    showInputColor(sharedCanvas.colorRed,sharedCanvas.colorGreen,sharedCanvas.colorBlue);
+  }); 
+  $('#canvasBlue').bind('input', function(){
+    checkInput(this, SharedCanvas.COLOR_BLUE);
+    showInputColor(sharedCanvas.colorRed,sharedCanvas.colorGreen,sharedCanvas.colorBlue);
+  });
+  $('#canvasCursorWidth').bind('input', function() {
+    sharedCanvas.cursorWidth = $('#canvasCursorWidth').val();
+  });
   
   
-  
-  /*Add onclick actions*/
-  
- 
-  
-  
-  /*Add listeners*/
-  canvas.addEventListener('mousedown',function (evt){
-  
-    var mousePos = getMousePos(canvas, evt);
-    var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
-    console.log(message);
-    point(mousePos.x,mousePos.y,canvas.getContext("2d"));
-  }, false);
+  /*Add Mouse listeners*/
+  canvas.addEventListener('mousedown', function(e){
+    sharedCanvas.onMouseDown(e);
+  });
+  canvas.addEventListener('mouseup', function(e){
+    sharedCanvas.onMouseUp(e);
+  });
+  canvas.addEventListener('mouseout', function(e){
+    sharedCanvas.onMouseUp(e);
+  });  
+  canvas.addEventListener('mousemove', function(e){
+    sharedCanvas.onMouseMove(e);
+  });
   
 });
 
-/*
-function canvasToolsPen(){
-  
-}*/
-
-function getMousePos(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
-  return {
-    x:  Math.round((evt.clientX-rect.left)/(rect.right-rect.left)*canvas.width),
-    y:  Math.round((evt.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height)
-  };
-}
-/*
-var canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
-
-// That's how you define the value of a pixel //
-function drawPixel (x, y, r, g, b, a) {
-    var index = (x + y * canvasWidth) * 4;
-
-    canvasData.data[index + 0] = r;
-    canvasData.data[index + 1] = g;
-    canvasData.data[index + 2] = b;
-    canvasData.data[index + 3] = a;
+function numbersOnly(val){
+  for(var i = 0; i < val.length; i++){
+    if(val[i]<'0' || val[i]>'9'){
+      return false;    
+    }
+  }
+  return true;
 }
 
-// That's how you update the canvas, so that your //
-// modification are taken in consideration //
-function updateCanvas() {
-    ctx.putImageData(canvasData, 0, 0);
-}*/
-
-function point(x, y, ctx){
-  ctx.strokeRect(x,y,1,1);
+function checkInput(obj, color){
+  var val = $(obj).val();
+  if (!numbersOnly(val)){
+    val = val.slice(0,val.length-1);
+  }
+  var res = sharedCanvas.changeColor(color, val); 
+  $(obj).val(res);  
 }
-/*
-var mousedownID = -1;  //Global ID of mouse down interval
-function mousedown(event) {
-  if(mousedownID==-1)  //Prevent multimple loops!
-     mousedownID = setInterval(whilemousedown, 100 /*execute every 100ms*/);
-
-
+function showInputColor(r,g,b){
+  var color = "RGB("+r+","+g+","+b+")";
+  $("#canvasResultColor").css("background-color",color)
 }
-function mouseup(event) {
-   if(mousedownID!=-1) {  //Only stop if exists
-     clearInterval(mousedownID);
-     mousedownID=-1;
-   }
 
-}
-function whilemousedown() {
-   /*here put your code*/
-}
-//Assign events
-document.addEventListener("mousedown", mousedown);
-document.addEventListener("mouseup", mouseup);
-//Also clear the interval when user leaves the window with mouse
-document.addEventListener("mouseout", mouseup);*/
 
 
