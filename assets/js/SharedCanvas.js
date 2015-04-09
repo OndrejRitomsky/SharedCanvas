69dbad6972bc2cf7277c9709f284fabed5e6f44a
OndrejRitomsky <o.ritomsky@gmail.com>
2015-04-09 17:05:38 +0200

    Sync finished, double drawing used to fix multiplelines problem

--- assets/js/SharedCanvas.js
+++ assets/js/SharedCanvas.js
@@ -22,11 +22,6 @@ function SharedCanvas(){
   this.saveToBuffer = false;
   
   this.oldCanvasState = null; // not setting state, but image! 
-  
-  this.lastActionMoveTo = null;
-  this.needReMoveTo = null;
-  this.reMoveToPos = {x:0,y:0};
-  
 }
 
 
@@ -46,7 +41,7 @@ SharedCanvas.COLOR_GREEN = "GREEN";
 SharedCanvas.COLOR_BLUE = "BLUE";
 
 
-
+// Initialization, instead of contructor... 
 SharedCanvas.prototype.initialization = function(canvas, io){
   this.io = io;
     
@@ -64,60 +59,50 @@ SharedCanvas.prototype.initialization = function(canvas, io){
 }
 
 
+//Update canvas with packages from server
 
-// uid vymazat
 SharedCanvas.prototype.updateCanvas = function(packages, uid){
-  //return;
-  this.context.save();
 
-  //if (this.lastActionMoveTo)
-  //  this.needReMoveTo=true;
-  // ---------- !!!!!!!!!!!!!!!!!!!!!!! ----------------
-  //this.context.putImageData(this.oldCanvasState, 0 , 0);
+  this.context.save();
 
   for(var p in packages){
     var package = packages[p].buffer;
     var puid = packages[p].uid;
     
-    
-    console.log(uid);
-    console.log(puid);
-    
-    
     var pos = {x:-1,y:-1};
     var i = 0;
-    //console.log(package);
-    this.context.beginPath();
+
     while(i < package.length){
+      
       if(package[i]=='MT'){
-        //this.context.beginPath();
-        //this.context.moveTo(package[i+1],package[i+2]);
         pos.x = package[i+1];
         pos.y = package[i+2];
         i += 3; 
         continue;
       } 
       if(package[i]=='LT'){
+        
         this.context.beginPath();
-        this.context.moveTo(pos.x, pos.y);
+        this.context.moveTo(pos.x, pos.y);        
         this.context.lineTo(package[i+1],package[i+2]);
+        this.context.stroke(); 
+        
         if (uid!=puid){
+          // if this wasnt packet from us, draw the same line twice, bcs if it is from us, it already drawn twice
+          this.context.beginPath();
           this.context.moveTo(pos.x, pos.y);
           this.context.lineTo(package[i+1],package[i+2]);
+          this.context.stroke(); 
         }
-        this.context.stroke(); 
+       
         pos.x = package[i+1];
         pos.y = package[i+2];
         i += 3;
         continue;
       } 
       if(package[i]=='S'){         
-        // settings out of beginpath?
-    
         this.context.lineWidth = package[i+1];  
         this.context.strokeStyle = 'rgba('+package[i+2]+','+package[i+3]+','+package[i+4]+',1)';
-    
-        //package[i+2]
         i += 5;        
         continue;
       } 
@@ -126,24 +111,20 @@ SharedCanvas.prototype.updateCanvas = function(packages, uid){
     } 
     
   }  
-   
-  
-  //this.oldCanvasState = this.context.getImageData(0, 0, SharedCanvas.CANVAS_WIDTH, SharedCanvas.CANVAS_HEIGHT);
-  
-
   this.context.restore();
 
 }
 
 
-  var sendId = 0;
+var sendId = 0;
 var getId = 0;
+
 
 SharedCanvas.prototype.changeCursorWidth = function(value){
   this.settingsChanged = true;
   this.cursorWidth = value;
 }
-// WIPES BUFFER!!
+// Prepare buffer to be in correct form for sending.
 SharedCanvas.prototype.getChangesBuffer = function(){
   var res = this.buffer;
   this.buffer = [];
@@ -163,41 +144,24 @@ SharedCanvas.prototype.getChangesBuffer = function(){
       var i = res.length-1;
       var ind = -1;
       while(i>=0){
-        if (res[i]=="LT"){
+        if (res[i]=="LT"|| res[i]=="MT"){
           ind = i;
           break;
         }
         i--;
       }
+      if (ind!=-1);
       this.addChangesToBuffer('MT', [res[ind+1],res[ind+2]]);
     
   }
-  
-  /* if last action was MT, we ll splice it out and add it to next buffer.*/
-  /*if (lastMT){
-
-    
-    if (ind>0){
-      var lastMTAction = res.splice(ind,3); 
-      this.addChangesToBuffer('MT', [lastMTAction[1],lastMTAction[2]]);
-     
-    }
-  }
-  lastMT = null;*/
 
 
   return res;
 }
-var lastMT = null;
+// Saves changed to network buffer.
 SharedCanvas.prototype.addChangesToBuffer = function(action, params){
   if (action!='MT' && action!='LT' && action!='S')
     return;
-  
-  if(action=='MT'){
-    lastMT = true;
-  } else if(action=='LT'){
-    lastMT = false;
-  } 
   
   this.buffer.push(action);
   for (var i = 0; i < params.length; i++){
@@ -229,80 +193,54 @@ SharedCanvas.prototype.changeColor = function(color, value){
   
 }
 
+//Calculates mouse position inside the canvas
 SharedCanvas.prototype.getMousePos = function(evt) {
   var rect = this.canvas.getBoundingClientRect();
   return {
     x:  Math.round((evt.clientX-rect.left)/(rect.right-rect.left)*canvas.width) +0.5,
     y:  Math.round((evt.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height) +0.5
-
   };  
-}
-
-
-SharedCanvas.prototype.moveTo = function(x, y){
-  if(this.saveToBuffer){
-    this.addChangesToBuffer('MT', [x, y]); 
-    
-  }
-  this.lastActionMoveTo = true;
-  this.reMoveToPos.x = x;
-  this.reMoveToPos.y = y;
-  this.context.moveTo(x, y); 
-  
 }
 
 SharedCanvas.prototype.lineTo = function(x, y){
   if(this.saveToBuffer){
     this.addChangesToBuffer('LT', [x, y]); 
   }
-  this.lastActionMoveTo = false;
   this.context.lineTo(x, y);   
 }
 
 
-
+//We are drawing, draw line and save position
 SharedCanvas.prototype.whilemousedown = function() {
 
   if (this.mouseEvt!=null){
   
     var pos = this.getMousePos(this.mouseEvt);
-    
+    this.mouseEvt = null;
     if (!this.firedOnce){
-      //this.context.beginPath();
-      this.moveTo(pos.x,pos.y);
+      if(this.saveToBuffer){
+        this.addChangesToBuffer('MT', [pos.x, pos.y]); 
+      } 
       this.counter = (this.counter + 1);// % 2;
       this.firedOnce = true;  
       this.oldPos = pos;
       return;
     }
     
-   /* if (this.counter==0){
-      this.context.beginPath();      
-      if (this.oldPos!=null){
-        this.moveTo(this.oldPos.x, this.oldPos.y);
-      } else {
-        this.moveTo(pos.x,pos.y);
-      }
-
-    }*/
-    
     if (this.counter>0){
       this.context.beginPath();      
       this.context.moveTo(this.oldPos.x, this.oldPos.y);
-      if(this.needReMoveTo){
-        this.context.moveTo(this.reMoveToPos.x,this.reMoveToPos.y);
-        this.needReMoveTo = false;
-      }
       this.lineTo(pos.x, pos.y);   
       this.context.stroke();
-      this.oldPos=pos;
-
+      this.oldPos=pos;      
     }
     
-    this.counter = (this.counter + 1);// % 2;
+    this.counter = (this.counter + 1);
   }
 }
 
+
+// We started drawing, set settings and save them to buffer if needed
 SharedCanvas.prototype.onMouseDown = function(event) {
   
   if(this.mousedownID==-1){
@@ -316,13 +254,13 @@ SharedCanvas.prototype.onMouseDown = function(event) {
        this.addChangesToBuffer('S', params);   
        this.settingsChanged = false;
      }
-    
-     // -------------------
+
      var t = this;
      this.mousedownID = setInterval(function(){t.whilemousedown();}, SharedCanvas.CURSOR_DRAWING_UPDATE_RATE);
   }
 }
 
+// WE stopped drawing, make sure whilemousedown fired at least once so dot is drawn.
 SharedCanvas.prototype.onMouseUp = function(event) {
   if(this.mousedownID!=-1) {  //Only stop if exists
      clearInterval(this.mousedownID);
@@ -337,27 +275,7 @@ SharedCanvas.prototype.onMouseUp = function(event) {
      this.firedOnce = false;
    }
 }
-
+// save mouse position
 SharedCanvas.prototype.onMouseMove = function(event) { 
   this.mouseEvt = event;
 }
-
-// ---------------------------------------
-/*
-var canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
-
-// That's how you define the value of a pixel //
-function drawPixel (x, y, r, g, b, a) {
-    var index = (x + y * canvasWidth) * 4;
-
-    canvasData.data[index + 0] = r;
-    canvasData.data[index + 1] = g;
-    canvasData.data[index + 2] = b;
-    canvasData.data[index + 3] = a;
-}
-
-// That's how you update the canvas, so that your //
-// modification are taken in consideration //
-function updateCanvas() {
-    ctx.putImageData(canvasData, 0, 0);
-}*/
